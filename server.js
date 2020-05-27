4f914d3737d9dd77f78783e650b20117eadf355f
Abshir <abshirm@live.com>
2020-05-27 13:02:11 -0700

    user authentication added

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init Middleware
+
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("HELLO WORLD!"));
+
+//Define Routes
+
+app.use("/api/users", require("./routes/api/users"));
+
+app.use("/api/auth", require("./routes/api/auth"));
+
+app.use("/api/profile", require("./routes/api/profile"));
+
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
