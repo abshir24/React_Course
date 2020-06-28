47771bff18cb60fcc56aa336e68550f43a8acb70
Abshir <abshirm@live.com>
2020-06-27 19:42:42 -0700

    transffering machine's

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -65,7 +67,22 @@ router.post(
 
       //Return jsonwebtoken
 
-      res.send("User Registered");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000000 },
+        (err, token) => {
+          if (err) throw err;
+
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
 
